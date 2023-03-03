36d096c3985ccb0bfae5e31aaef3bdc720244cd9
zoomviex <agvazvinicius@gmail.com>
2023-03-02 21:05:13 -0300

    continuação aula 10

--- js/exercicio 30.js
+++ js/exercicio 30.js
@@ -1,39 +1,62 @@
-function enviarAvaliacao() {
-  let nome = document.getElementById("nome").value;
-  let avaliacao1 = parseFloat(document.getElementById("avaliacao1").value);
-  let avaliacao2 = parseFloat(document.getElementById("avaliacao2").value);
+const aluno = {
+  nome: "",
+  matricula: "",
+  turma: "",
+  avaliacao1: 0,
+  avaliacao2: 0,
+  media: 0,
+  resultado: ""
+}
 
-  let errosEncontrados = validarDados(nome, avaliacao1, avaliacao2);
+function enviarAvaliacao() {
+  aluno.nome = document.getElementById("nome").value;
+  //aluno.matricula = document.getElementById("matricula").value;
+  //aluno.turma = document.getElementById("turma").value;
+  aluno.avaliacao1 = parseFloat(document.getElementById("avaliacao1").value);
+  aluno.avaliacao2 = parseFloat(document.getElementById("avaliacao2").value);
+
+  let errosEncontrados = validarDados(aluno);
   if (errosEncontrados != "") {
       alert(errosEncontrados);
       return
   }
 
-  let media = calculoMedia(avaliacao1, avaliacao2);
+  let media = calculoMedia(aluno);
+  aluno.media = media;
   let resultado = montarResultado(media);
+  aluno.resultado = resultado;
 
-  document.getElementById("saidaTexto").innerHTML = "Resultado: " + resultado + " - Media: " + media;
+  //document.getElementById("saidaTexto").innerHTML += "<tr><td>" + nome + "</td><td>" + resultado + "</td><td>" + media + "</td><tr>";
+  document.getElementById("saidaTexto").innerHTML += `<tr><td>${aluno.nome}</td><td>${aluno.resultado}</td><td>${aluno.media}</td><tr>`;
+
+  // document.cookie = "aluno=" + JSON.stringify(aluno);
+  // document.cookie = "busca=" + aluno.nome;
+  // let dadoscookie = document.cookie;
+
+  localStorage.setItem("aluno", JSON.stringify(aluno));
+  let dadosAluno = JSON.parse(localStorage.getItem("aluno"));
+  console.log(dadosAluno);
 }
 
-function validarDados(nomeAluno = "", avalicao1Aluno = 0, avalicao2Aluno = 0) {
+function validarDados(aluno = {}) {
   let erros = "";
-  if (!nomeAluno) {
+  if (aluno.none) {
       erros += "Nome em branco.\n";
   }
 
-  if (avalicao1Aluno < 0 || avalicao1Aluno > 10 || Number.isNaN(avalicao1Aluno)) {
+  if (aluno.avaliacao1 < 0 || aluno.avaliacao1 > 10 || Number.isNaN(aluno.avaliacao1)) {
       erros += "Valor da Avaliação 1 esta fora do padrão (0,0 a 10,0).\n";
   }
 
-  if (avalicao2Aluno < 0 || avalicao2Aluno > 10 || Number.isNaN(avalicao2Aluno)) {
+  if (aluno.avaliacao2 < 0 || aluno.avaliacao2 > 10 || Number.isNaN(aluno.avaliacao2)) {
       erros += "Valor da Avaliação 2 esta fora do padrão (0,0 a 10,0).\n";
   }
 
   return erros;
 }
 
-function calculoMedia(avaliacao1Aluno = 0, avaliacao2Aluno = 0) {
-  return (avaliacao1Aluno + avaliacao2Aluno) / 2;
+function calculoMedia(aluno) {
+  return (aluno.avaliacao1 + aluno.avaliacao2) / 2;
 }
 
 function montarResultado(mediaAluno = 0) {
@@ -46,4 +69,6 @@ function montarResultado(mediaAluno = 0) {
   }
 
 }
+
+
 //ToDo: executar e analisar a divisão de responsabilidade de cada função.
